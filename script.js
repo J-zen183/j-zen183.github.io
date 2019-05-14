572e85847f58c0b76dc5414e5d344a38b159cd6a
J-zen183 <50493112+J-zen183@users.noreply.github.com>
2019-05-14 17:24:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function chooseV() {
+  var themePage = document.getElementById("chooseTheme");
+  themePage.style.display = "none";
 
+  var air = document.getElementById("air");
+  var jungle = document.getElementById("jungle");
+  var water = document.getElementById("water");
+
+  if (air) {
+    document.getElementById("airVehicle").hidden = false;
+  } else if (jungle) {
+    document.getElementById("jungleVehicle").hidden = false;
+  } else if (water) {
+     document.getElementById("waterVehicle").hidden = false;
+  }
+}
+
+function planeStart() {
+
+}
+function jetStart() {
+
+}
